60ae3d7967d8773fb5a8137541ee6e2681d8b5ce
tuan311020022 <nguyen.minhtuan@hachi-x.com>
2023-06-26 07:28:40 +0700

    refactor folder structure

---
+++ server/callback.php
@@ -0,0 +1,80 @@
+<?php
+header('Access-Control-Allow-Origin: http://localhost:3000');
+require_once("./function.php");
+include("./configs/conn.php");
+
+        /// status = 1 ==> thẻ đúng
+        /// status = 2 ==> thẻ sai
+        /// status = 3 ==> thẻ ko dùng đc
+        /// status = 99 ==> thẻ chờ xử lý
+    $mathe = $_POST['code'];
+    $seri =  $_POST['serial'];
+    $amount =  $_POST['amount'];
+    $type =  $_POST['telco'];
+    $code = $_POST['request_id'];
+    $sign =$_POST['sign'];
+    $id = '78308817836';
+    $idchar = $_POST['idchar'];
+    $key = '258d4d1f29d488effbfca07fcb016e1e';
+    $url = 'https://thesieure.com/chargingws/v2?sign='.$sign.'&telco='.$type.'&code='.$mathe.'&serial='.$seri.'&amount='.$amount.'&request_id='.$code.'&partner_id='.$id.'&command=check';
+    $process = json_decode(curl_get($url), true);
+    $status = $process['status'];
+    if($status == 1) {
+        $account = mysqli_fetch_assoc(mysqli_query($connect,"SELECT * FROM `player` WHERE `name`='".$_POST['idchar']."'"));
+        $charge = mysqli_fetch_assoc(mysqli_query($connect,"SELECT * FROM `napthe` WHERE `sign`='".$sign."'"));
+        if($charge) {
+            mysqli_close($connect);
+            echo json_encode(
+                [
+                "check" => 3,
+                "status" => $status
+                ]
+            );
+        }
+        else {
+            $sql = "UPDATE `player` SET `recharge`=`recharge` +'{$amount}' WHERE`name`='".$_POST['idchar']."'";
+            if(mysqli_query($connect,$sql)) {
+                
+            }
+            
+            if(mysqli_query($connect,$sql = "UPDATE `player` SET `donate`=`donate`+'{$amount}' WHERE `name`='".$_POST['idchar']."'")) {
+                
+            }
+
+            if(mysqli_query($connect,$sql = "UPDATE `player` SET `point`=`point`+'{$amount}' WHERE `name`='".$_POST['idchar']."'")) {
+                
+            }
+
+            if(mysqli_query($connect,"UPDATE `account` SET `active` = 1  WHERE `id`='".$account['account_id']."'")) {
+                $smg = $account['account_id'];
+            }
+            mysqli_query($connect,"INSERT INTO `napthe` (idChar,amount,sign) VALUES('$idchar','$amount','$sign')");
+            mysqli_close($connect);
+            echo json_encode(
+                [
+                "check" => 3,
+                "status" => $status
+                ]
+            );
+        }
+    }
+    elseif($status == 99) {
+        mysqli_close($connect);
+            echo json_encode(
+                [
+                "check" => 1,
+                "status" => $status
+                ]
+            );
+    } 
+    else {
+        mysqli_close($connect);
+            echo json_encode(
+                [
+                "check" => 2,
+                "status" => $status
+                ]
+            );   
+    }
+
+?>
---
+++ server/recharge.php
@@ -0,0 +1,95 @@
+<?php
+header('Access-Control-Allow-Origin: http://localhost:3000');
+require_once("./function.php");
+include("./configs/conn.php");
+
+    $idchar = $_POST['idchar'];
+    $mathe = $_POST['code'];
+    $seri =  $_POST['serial'];
+    $amount =  $_POST['amount'];
+    $type =  $_POST['telco'];
+    if(empty($type))
+    {
+        mysqli_close($connect);
+        echo json_encode(
+            [
+            "check" => 1
+            ]
+        );
+    }
+    elseif(empty($amount))
+    {
+        mysqli_close($connect);
+        echo json_encode(
+            [
+            "check" => 1
+            ]
+        );
+    }
+    elseif(empty($seri))
+    {
+        mysqli_close($connect);
+        echo json_encode(
+            [
+            "check" => 1
+            ]
+        );
+    }
+    elseif(empty($mathe))
+    {
+        mysqli_close($connect);
+        echo json_encode(
+            [
+            "check" => 1
+            ]
+        );
+    }
+    elseif (strlen($seri) < 5 || strlen($mathe) < 5)
+    {
+        mysqli_close($connect);
+        echo json_encode(
+            [
+            "check" => 1
+            ]
+        );
+    }
+    else {
+        $account = mysqli_fetch_assoc(mysqli_query($connect,"SELECT * FROM `player` WHERE `name`='".$idchar."'"));
+        if($account){
+            $code = rand(100000000, 999999999);
+            $id = '78308817836';
+            $key = '258d4d1f29d488effbfca07fcb016e1e';
+            $url = 'https://thesieure.com/chargingws/v2?sign='.md5($key.$mathe.$seri).'&telco='.$type.'&code='.$mathe.'&serial='.$seri.'&amount='.$amount.'&request_id='.$code.'&partner_id='.$id.'&command=charging';
+            $xuly = json_decode(curl_get($url), true);
+            $callback_sign = md5($key.$mathe.$seri);
+            if($xuly['status'] == 99)
+            {
+              $url = 'https://nroshine.online/shine/callback.php?sign='.md5($key.$mathe.$seri).'&telco='.$type.'&idchar='.$idchar.'&code='.$mathe.'&serial='.$seri.'&amount='.$amount.'&request_id='.$code.'&partner_id='.$id.'&command=check';
+              mysqli_close($connect);
+              echo json_encode(
+                [
+                "check" => 3,
+                "key" => $code,
+                "sign" => $callback_sign
+                ]
+            );
+            }
+            else {
+                mysqli_close($connect);
+                echo json_encode(
+                    [
+                    "check" => 2
+                    ]
+                );
+            }
+        } else {
+            mysqli_close($connect);
+            echo json_encode(
+                [
+                "check" => 4
+                ]
+            );
+        }
+    }
+    
+?>
